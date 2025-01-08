691c9cc4fcaef4733fba53b2b4ee87c202620f23
TheOrangeHoodie <supremethedev@gmail.com>
2025-01-08 08:41:44 +0100

    Added a faction create command

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ config = dotenv.dotenv_values("./.env")
 intents = nextcord.Intents.all()
 client = commands.Bot(intents=intents, command_prefix="o!")
 
+OWNER = 883030668962066452
+
 # On start
 @client.event
 async def on_ready():
@@ -19,7 +21,7 @@ for fn in os.listdir("./modules"):
 
 @client.command()
 async def load(ctx, extenstion):
-    if not ctx.author.id == 883030668962066452:
+    if not ctx.author.id == OWNER:
         ctx.send("You dont have permissions to use this command!")
         return
     client.load_extension(extenstion)
@@ -27,7 +29,7 @@ async def load(ctx, extenstion):
 
 @client.command()
 async def unload(ctx, extenstion):
-    if not ctx.author.id == 883030668962066452:
+    if not ctx.author.id == OWNER:
         ctx.send("You dont have permissions to use this command!")
         return
     client.unload_extension(extenstion)
@@ -35,7 +37,7 @@ async def unload(ctx, extenstion):
 
 @client.command()
 async def reload(ctx, extenstion):
-    if not ctx.author.id == 883030668962066452:
+    if not ctx.author.id == OWNER:
         ctx.send("You dont have permissions to use this command!")
         return
     client.reload_extension(extenstion)
---
+++ modules/admin.py
@@ -0,0 +1,35 @@
+import nextcord
+from nextcord.ext import commands
+
+admins = [883030668962066452]
+
+class Admin(commands.Cog):
+    def __init__(self, bot : commands.Bot):
+        self.client = bot
+
+    @nextcord.slash_command(name="factioncreate", description="Admin command to create a faction", guild_ids=[1325892979646267404])
+    async def factionCreate(self,
+                            ctx : nextcord.Interaction,
+                            name: str = nextcord.SlashOption(name="name", description="Faction name"),
+                            description: str = nextcord.SlashOption(name="description", description="Faction description"),
+                            role: nextcord.Role = nextcord.SlashOption(name="role", description="Faction Role")
+                            ):
+        if ctx.user.id not in admins:
+            await ctx.send("You dont have permissions to use this command! If this is a mistake contact TheOrangeHoodie aka Supreme")
+            return
+        
+        confirmEmbed = nextcord.Embed(
+            title=f"Faction: {name}",
+            description=f"""Faction Description: {description}
+                            Role: {role.mention}""",
+            color=role.color
+        )
+
+        await ctx.send("Please confirm faction creation.", embed=confirmEmbed)
+        
+
+
+    
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
